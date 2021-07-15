a5be9696b79408d922f96c3fc03a14ed7578dcd7
cercenazi <celil.cercenazi@gmail.com>
2021-07-15 21:52:07 +0300

    remove the vue cli code and add custom inittial code

---
+++ youtube-comment-language/src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <h2>{{appName}}</h2>
+</template>
+
+<script>
+
+export default ({
+    name : "Home",
+    data: function(){
+        return{
+            appName: "Cool App"
+        }
+    }
+})
+</script>
